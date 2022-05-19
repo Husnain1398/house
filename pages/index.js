67ac6f0162191f4f2b9fc3f8064af85663bf5342
Husnain1398 <99725528+Husnain1398@users.noreply.github.com>
2022-05-19 20:45:39 +0500

    chiut

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import dynamic from 'next/dynamic';
 import Link from 'next/link'
 const Qrcode =
-  dynamic(() => import("../components/qrcode/Qrcode"), { ssr: false });
+  dynamic(() => import("../Components/qrcode/Qrcode"), { ssr: false });
 
 
 export default function Home() {
